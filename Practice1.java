c75fd60852d73f2c54a68ac653a0a451e973c936
shashankai2007-star <shashank.ai2007@gmail.com>
2026-03-10 13:20:47 +0530

    Practice1

---
+++ Practice1.java
@@ -0,0 +1,10 @@
+public class Practice1 {
+public static void main(String[]args){
+        int dob=2000;
+        int present=2024;
+        System.out.println("Hary's age in "+present+" is "+(present-dob));
+
+    }
+}
+    
+
