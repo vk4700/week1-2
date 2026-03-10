a71bc86f3ffc2ef1799fd46213759fb70e827bf5
shashankai2007-star <shashank.ai2007@gmail.com>
2026-03-10 13:50:52 +0530

    Practice3

---
+++ Practice3.java
@@ -0,0 +1,8 @@
+public class Practice3 {
+public static void main(String[]args){
+        float dist=10.8f;
+        float mile =(dist*1.6f);
+        System.out.format("The distance 10.8 km in miles is %.2f",mile);
+    }
+    
+}
