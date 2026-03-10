d16178f0235f5b9646d4b5c3b2642187653ceeaf
shashankai2007-star <shashank.ai2007@gmail.com>
2026-03-10 13:38:24 +0530

    practice2

---
+++ Practice2.java
@@ -0,0 +1,13 @@
+public class Practice2 {
+public static void main(String[]args){
+        float math,phy,chem;
+        math=94;
+        phy=95;
+        chem=96;
+        System.out.println("Sam's average mark in PCM is "+((math+phy+chem)/3));
+
+
+    }
+    
+}
+
