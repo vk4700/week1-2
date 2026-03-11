f478d1e9ffdea151663bf431e0376769dd1d0b79
shashankai2007-star <shashank.ai2007@gmail.com>
2026-03-11 14:00:45 +0530

    practice21

---
+++ Practice21.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Practice21 {
+public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        float sal,bonus,total;
+        System.out.println("Enter the Salary:");
+        sal=sc.nextFloat();
+        System.out.println("Enter bonus:");
+        bonus=sc.nextFloat();
+        total=sal+bonus;
+        System.out.println(String.format("The Salary is INR %.2f and bonus is INR %.2f.Hence Total Income is INR %.2f",sal,bonus,total));
+        sc.close();
+    
+
+    }
+    
+}
